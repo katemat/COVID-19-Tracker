68d5cf47fcc0ec7dd33a65bde321109235a9202e
Katerina <matveevakaterinka@gmail.com>
2020-08-10 22:10:36 +1000

    added flag to table

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,8 +5,11 @@ import numeral from 'numeral';
 function Table({ countries }) {
   return (
     <div className="table">
-      {countries.map(({ country, cases }) => (
+      {countries.map(({ country, cases, countryInfo }) => (
         <tr>
+          <td>
+            <img className="table__flag" src={countryInfo.flag} alt="" />
+          </td>
           <td>{country}</td>
           <td>
             <strong>{numeral(cases).format('0,0')}</strong>
